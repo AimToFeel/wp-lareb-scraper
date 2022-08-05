57db2f4bec8eb368ff89ff82640ba22d56606bdd
Niek van der Velde <niek@velde.xyz>
2022-08-05 16:18:22 +0200

    Add data scraper

--- src/WPLarebScraper.php
+++ src/WPLarebScraper.php
@@ -2,8 +2,12 @@
 
 namespace WPLarebScraper\src;
 
+use DOMDocument;
+use DOMXPath;
+
 class WPLarebScraper
 {
+    private $tableName;
 
     /**
      * On plugin init.
@@ -15,6 +19,44 @@ class WPLarebScraper
      */
     public function onInit(): void
     {
+        wp_schedule_single_event(time(), 'wp_lareb_scraper_scrape');
+        if (!wp_next_scheduled('wp_lareb_scraper_scrape')) {
+            // wp_schedule_event(time(), 'daily', 'wp_lareb_scraper_scrape');
+        }
+
+        global $wpdb;
+        $this->tableName = "{$wpdb->prefix}lareb_scraper";
+    }
+
+    /**
+     * Start craping Lareb.
+     *
+     * @return void
+     *
+     * @author Niek van der Velde <niek@aimtofeel.com>
+     * @version 1.0.0
+     */
+    public function scrape(): void
+    {
+        global $wpdb;
+
+        $response = wp_remote_get('https://www.lareb.nl/coronameldingen');
+
+        if (is_array($response) && !is_wp_error($response)) {
+            $body = $response['body'];
+
+            $document = new DOMDocument();
+            $document->loadHTML($body);
+            $xPath = new DOMXPath($document);
+
+            $contentNodeOne = $xPath->query('//div[@id="CntntPage_1"]//p')->item(0);
+            $paragraphOne = $contentNodeOne->nodeValue;
+
+            var_dump($paragraphOne);
+
+            $wpdb->update($this->tableName, ['value' => strlen($body)], ['name' => 'covid_side_effects']);
+            $wpdb->update($this->tableName, ['value' => strlen($body)], ['name' => 'covid_deaths']);
+        }
 
     }
 }
--- wp-lareb-scraper.php
+++ wp-lareb-scraper.php
@@ -22,3 +22,61 @@ require_once WP_SWIFT_MAILER_DIRECTORY . 'src/WPLarebScraperException.php';
 
 $wpLarebScraper = new WPLarebScraper();
 add_action('init', [$wpLarebScraper, 'onInit']);
+add_action('wp_lareb_scraper_scrape', [$wpLarebScraper, 'scrape']);
+
+/**
+ * On plugin register.
+ *
+ * @return void
+ *
+ * @author Niek van der Velde <niek@aimtofeel.com>
+ * @version 1.0.0
+ */
+function activate_wp_larab_scraper(): void
+{
+    global $wpdb;
+    $tableName = "{$wpdb->prefix}lareb_scraper";
+    $charsetCollate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $tableName (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        name varchar(255) NOT NULL,
+        value int NOT NULL DEFAULT 0,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY  (id)
+    ) $charsetCollate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta($sql);
+
+    $wpdb->insert($tableName, [
+        'name' => 'covid_side_effects',
+        'value' => 0,
+    ]);
+
+    $wpdb->insert($tableName, [
+        'name' => 'covid_deaths',
+        'value' => 0,
+    ]);
+}
+
+/**
+ * On uninstall.
+ *
+ * @return void
+ *
+ * @author Niek van der Velde <niek@aimtofeel.com>
+ * @version 1.0.0
+ */
+function deactivate_wp_larab_scraper(): void
+{
+    global $wpdb;
+    $tableName = "{$wpdb->prefix}lareb_scraper";
+    $sql = "DROP TABLE $tableName;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta($sql);
+}
+
+register_activation_hook(__FILE__, 'activate_wp_larab_scraper');
+register_deactivation_hook(__FILE__, 'deactivate_wp_larab_scraper');
